408101c27fd09b0338473723ff3a5654ffc74638
Araujo-Henrique <henriquesudaraujo@gmail.com>
2026-09-08 22:21:58 -0300

    feat: adicionado método para atualizar clientes juntamente com alguns comentários no código

--- app/main.py
+++ app/main.py
@@ -17,10 +17,24 @@ class Cliente(BaseModel):
     situacao: str
     resultado: str
 
+class ClienteUpdate(BaseModel):
+    nome: str | None = None
+    email: str | None = None
+    telefone: str | None = None
+    n_solicitantes: int | None = None
+    data_casv: str | None = None
+    data_entrevista: str | None = None
+    consulado: str | None = None
+    status_rascunho: str | None = None
+    status_ds: str | None = None
+    orientacao: str | None = None
+    situacao: str | None = None
+    resultado: str | None = None
+
 clientes = []
 cliente_id = 1
 
-
+#Cria novos clientes
 @app.post("/clientes")
 def cria_cliente(cliente: Cliente):
     global cliente_id
@@ -31,14 +45,16 @@ def cria_cliente(cliente: Cliente):
     }
 
     clientes.append(novo_cliente)
-    cliente_id +=1
+    cliente_id += 1
 
     return novo_cliente
 
+#Busca lista de clientes registrados
 @app.get("/clientes")
 def busca_clientes():
     return clientes
 
+#Busca um cliente específico
 @app.get("/clientes/{cliente_id}")
 def busca_cliente_por_id(cliente_id: int):
     for cliente in clientes:
@@ -50,6 +66,7 @@ def busca_cliente_por_id(cliente_id: int):
         detail="Cliente não encontrado"
     )
 
+#Deleta um cliente específico
 @app.delete("/clientes/{cliente_id}")
 def deleta_cliente_por_id(cliente_id: int):
     for index, cliente in enumerate(clientes):
@@ -61,3 +78,21 @@ def deleta_cliente_por_id(cliente_id: int):
             status_code=404,
             detail="Cliente não encontrado"
         )
+
+#Atualiza os dados de um cliente específico
+@app.patch("/clientes/{cliente_id}")
+def atualizar_cliente(cliente_id: int, dados: ClienteUpdate):
+
+    for cliente in clientes:
+        if cliente["id"] == cliente_id:
+            dados_atualizados = dados.model_dump(exclude_unset=True)
+
+            for campo, valor in dados_atualizados.items():
+                cliente[campo] = valor
+
+            return cliente
+
+    raise HTTPException(
+        status_code=404,
+        detail="Cliente não encontrado"
+    )
